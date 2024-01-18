f252df81d1e342a39335cb9c82395ce0c88d3d40
joao neto <joaonetotfm@gmail.com>
2024-01-17 21:00:08 -0300

    fix: corrige bug angular material

--- modules/consult-diets/src/lib/consult-diets/consult-diets.component.ts
+++ modules/consult-diets/src/lib/consult-diets/consult-diets.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UiComponent } from '@dietfactor/modules/shared/ui';
 
 @Component({
   selector: 'dietfactor-consult-diets',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, UiComponent],
   templateUrl: './consult-diets.component.html',
   styleUrl: './consult-diets.component.scss',
 })
--- modules/shared/ui/src/lib/ui/ui.component.ts
+++ modules/shared/ui/src/lib/ui/ui.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {MatButtonModule} from '@angular/material/button';
+
+
 @Component({
   selector: 'dietfactor-ui',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './ui.component.html',
   styleUrl: './ui.component.scss',
 })
