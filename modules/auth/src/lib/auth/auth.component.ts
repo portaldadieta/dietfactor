f232e28503bd01ba7e157a357d7ccd7a77684274
Victor Alexandre <varm@ic.ufal.br>
2024-01-20 11:20:40 -0300

    feat: Finaliza a versão inicial da tela de login.

--- modules/auth/src/lib/auth/auth.component.ts
+++ modules/auth/src/lib/auth/auth.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'dietfactor-auth',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    MatButtonModule,
+    CommonModule,
+    MatInputModule,
+    MatCardModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
 })
-export class AuthComponent {}
+export class AuthComponent {
+  constructor(private router: Router) {}
+
+  /* botão temporariamente redirecionando para a home */
+  redirectToRoute() {
+    this.router.navigate(['/']);
+  }
+}
