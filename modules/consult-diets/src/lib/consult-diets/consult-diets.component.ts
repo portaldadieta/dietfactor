06b4924a6a64276b189c41ad24c7a247e1fb7e6c
Caio Rocha <rochacaiodev@gmail.com>
2024-01-22 16:47:36 -0300

    feat: implementar melhorias no layout e mensagem informativo para o usuário quando dietas não forem encontradas

--- modules/consult-diets/src/lib/consult-diets/consult-diets.component.ts
+++ modules/consult-diets/src/lib/consult-diets/consult-diets.component.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { NavbarComponent } from '@dietfactor/modules/navbar';
+
 const MATERIAL_MODULES = [
   MatFormFieldModule,
   MatInputModule,
