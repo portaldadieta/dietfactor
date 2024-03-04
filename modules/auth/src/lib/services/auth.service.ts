0f31f87ec9c52f7c4a89522c26a93306c76b5ebc
= <=>
2024-03-04 18:05:12 -0300

    feat: implementa os métodos de manipulação do token e login

---
+++ modules/auth/src/lib/services/auth.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IAuth } from '../interfaces/IAuth.interface';
+import { catchError, tap, throwError } from 'rxjs';
+import { IResponse } from '../interfaces/IResponse.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private httpClient: HttpClient) {}
+
+  static dietFactorURL = 'http://localhost:3000';
+
+  setToken(token: string): void {
+    sessionStorage.setItem('token', token);
+  }
+
+  getToken(): string | null {
+    return sessionStorage.getItem('token');
+  }
+
+  clearToken(): void {
+    sessionStorage.removeItem('token');
+  }
+
+  login(data: IAuth): void {
+    this.httpClient
+      .post<IResponse>(`${AuthService.dietFactorURL}/auth/login`, data)
+      .pipe(
+        tap((data) => {
+          this.setToken(data.access_token);
+        }),
+        catchError((error) => {
+          console.log(`Ocorreu um erro: ${error}`);
+          return throwError(() => new Error(error));
+        })
+      )
+      .subscribe();
+  }
+}
